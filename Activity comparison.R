4326e7b6211ceb5cb27741648f3d2004c54c64f3
johndcusack <john.cusack@nhs.net>
2023-06-16 17:07:15 +0100

    Update Activity comparison.R

--- Activity comparison.R
+++ Activity comparison.R
@@ -16,3 +16,35 @@ source('helper_files//lookups.R')
 #set everything to lower case 
 source('helper_files//all_to_lower.R')
 
+month_commencing <- calendar %>% 
+  filter(day_num == 1) %>% 
+  select(date,
+         year_num,
+         month_name_short,
+         fin_year,
+         fin_month)
+
+combined <- left_join(x = actuals_3_years,
+                      y = month_commencing,
+                      by = join_by(month_commencing == date),
+                      keep = FALSE)
+
+combined <- combined %>% 
+  select(month_commencing,
+         year_num,
+         fin_year,
+         month_name_short,
+         fin_month,
+         der_provider_code,
+         stp_code,
+         activity) %>% 
+  mutate(der_provider_code = substr(der_provider_code,1,3)) %>% 
+  group_by(month_commencing,
+           year_num,
+           fin_year,
+           month_name_short,
+           fin_month,
+           der_provider_code,
+           stp_code) %>% 
+  summarise(activity = sum(activity)) %>% 
+  ungroup()
